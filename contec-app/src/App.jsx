008173f95e0d3f0d20309d54c5eb83b5b38b8fb1
VictordeConto <vitorneymarof87@gmail.com>
2025-06-16 22:02:12 -0300

    Atualização de funcionalidades

--- contec-app/src/App.jsx
+++ contec-app/src/App.jsx
@@ -2,19 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './pages/login/login.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import Cadastro from './pages/register/register.jsx'
+import Header from './components/header.jsx'
 
 function App(){
   return(
     <Router>
       <Routes>
         <Route path="/" element={<Login />}></Route>
-        <Route path="/register" element={<Cadastro />}/>
+        <Route path="/register" element={<Layout />}/>
       </Routes>
     </Router>
   )
 }
+
+function Layout() {
+  return (
+    <div>
+      {<Header />}
+      <main>
+        <Outlet /> {<Cadastro />}
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
